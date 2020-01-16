f6b8a275048b79c67afcac312225ae1d5daa0250
川阪佐奈恵 <sanae.kawasaka@gmail.com>
2020-01-16 23:55:19 +0900

    no message

--- dbconnect.php
+++ dbconnect.php
@@ -1,11 +1,11 @@
 <?php
 
 //DBに接続
-$host = getenv('localhost'); //MySQLがインストールされてるコンピュータ
-$dbname = getenv('form'); //使用するDB
+$host = 'localhost'; //MySQLがインストールされてるコンピュータ
+$dbname = 'form'; //使用するDB
 $charset = "utf8"; //文字コード
-$user = getenv('root'); //MySQLにログインするユーザー名
-$password= getenv('');//ユーザーのパスワード
+$user = 'root'; //MySQLにログインするユーザー名
+$password= '';//ユーザーのパスワード
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //SQLでエラーが表示された場合、画面にエラーが出力される
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //DBから取得したデータを連想配列の形式で取得する
